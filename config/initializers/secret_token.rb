cbf4f47001152388a226613f42dc52049d1fdc92
simcap <caplette@hotmail.co.uk>
2013-09-01 10:32:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EloquentFeed::Application.config.secret_token = '847dc94afd3c1a3e48b702c82e2ca258e652ed3a91181962baca97af84a46c13d00ef486a54caf93980011d2c80433b5da55e248aef392a805c6aea92a742a9a'
